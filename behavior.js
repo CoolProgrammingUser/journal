0e9d5ad2e820b732a3bdb4e8c5cb88afe9b2291d
Robert <ambitiousbusinessowner@gmail.com>
2017-10-29 00:55:40 -0500

    turned names into links

--- behavior.js
+++ behavior.js
@@ -2,13 +2,19 @@ var S = Standards;
 var people = [];
 
 S.onLoad(function() {
+	// fills all of the empty person references with people
 	people.forEach(function(person, index) {
 		S.getClass("p" + (index+1)).forEach(function(occurrence) {
+			let link = document.createElement("a");
+			link.className = "discreet";
+			link.href = "/journal/people";  // "/journal/" is included just in case the link is put in a weird place
+			//// see if I can put the person's reference in the URL
 			if (occurrence.classList.contains("full-name")) {
-				occurrence.innerHTML = person.fullName;
+				link.innerHTML = person.fullName;
 			} else {
-				occurrence.innerHTML = person.name;
+				link.innerHTML = person.name;
 			}
+			occurrence.appendChild(link);
 		});
 	});
 });
