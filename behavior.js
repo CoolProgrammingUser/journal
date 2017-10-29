bad05e5d6a1472a04fea62713d57c4a894f317fd
Robert <ambitiousbusinessowner@gmail.com>
2017-10-29 00:27:51 -0500

    added people interpretation

--- behavior.js
+++ behavior.js
@@ -1,1 +1,14 @@
-/* I need to add stuff here. */
+var S = Standards;
+var people = [];
+
+S.onLoad(function() {
+	people.forEach(function(person, index) {
+		S.getClass("p" + (index+1)).forEach(function(occurrence) {
+			if (occurrence.classList.includes("full-name")) {
+				occurrence.innerHTML = person.fullName;
+			} else {
+				occurrence.innerHTML = person.name;
+			}
+		});
+	});
+});
