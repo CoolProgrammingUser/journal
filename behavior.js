ba79bd8ade5ac90a20e1ee5a066f1bcdef025829
Robert <ambitiousbusinessowner@gmail.com>
2017-12-02 04:02:09 -0600

    updated and optimized behavior and formatting

--- behavior.js
+++ behavior.js
@@ -3,13 +3,16 @@ var people = [];
 
 S.onLoad(function() {
 	// fills all of the empty person references with people
-	people.forEach(function(person, index) {
-		S.getClass("p" + (index+1)).forEach(function(occurrence) {
+	S.forEach(people, function(person, index) {
+		S.forEach(S.getClass("p" + (index+1)), function(occurrence) {
 			let link = document.createElement("a");
 			link.className = "discreet";
 			link.href = "/journal/people";  // "/journal/" is included just in case the link is put in a weird place
 			//// see if I can put the person's reference in the URL
-			if (occurrence.classList.contains("full-name")) {
+			if (occurrence.innerHTML.trim() != "") {
+				link.innerHTML = occurrence.innerHTML;
+				occurrence.innerHTML = "";
+			} else if (occurrence.classList.contains("full-name")) {
 				link.innerHTML = person.fullName;
 			} else {
 				link.innerHTML = person.name;
