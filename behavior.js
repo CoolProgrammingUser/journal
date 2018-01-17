adf08228516958ae0df39b79bab2b1551d9f7642
Robert <ambitiousbusinessowner@gmail.com>
2018-01-17 11:48:39 -0600

    added refinements and content
    * expanded a planned entry
    * made minor revisions to the planned entry and people
    * added a notes page with a table of balloon animal dogs
    * put the person number in their name link
    * refined displayed name selection

--- behavior.js
+++ behavior.js
@@ -1,4 +1,4 @@
-var S = Standards;
+var S = Standards.general;
 var people = [];
 
 S.onLoad(function() {
@@ -7,13 +7,12 @@ S.onLoad(function() {
 		S.forEach(S.getClass("p" + (index+1)), function(occurrence) {
 			let link = document.createElement("a");
 			link.className = "discreet";
-			link.href = "/journal/people";  // "/journal/" is included just in case the link is put in a weird place
-			//// see if I can put the person's reference in the URL
+			link.href = "/journal/people?p=" + (index+1);  // "/journal/" is included just in case the link is put in a weird place
 			if (occurrence.innerHTML.trim() != "") {
 				link.innerHTML = occurrence.innerHTML;
 				occurrence.innerHTML = "";
-			} else if (occurrence.classList.contains("full-name")) {
-				link.innerHTML = person.fullName;
+			} else if (occurrence.dataset.use) {
+				link.innerHTML = person[occurrence.dataset.use];
 			} else {
 				link.innerHTML = person.name;
 			}
