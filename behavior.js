734b6937d55fa94b39922a40f9e04b65e2efe800
Robert <ambitiousbusinessowner@gmail.com>
2018-05-23 00:45:05 -0500

    made a few refinements and a lot of plans

--- behavior.js
+++ behavior.js
@@ -3,18 +3,23 @@ var people = [];
 
 S.onLoad(function() {
 	// fills all of the empty person references with people
-	S.forEach(people, function(person, index) {
-		S.forEach(S.getClass("p" + (index+1)), function(occurrence) {
+	let HTML = document.body.innerHTML;
+	HTML = HTML.replace(/([^'"“])(p\d+)([^'"”])/g, function (match, leftCharacter, person, rightCharacter) {
+		return leftCharacter + '<span class="' + person + '"></span>' + rightCharacter;
+	});
+	document.body.innerHTML = HTML;
+	S.forEach(people.slice(1), function(person, index) {
+		S.forEach(S.getClass("p" + index), function(occurrence) {
 			let link = document.createElement("a");
 			link.className = "discreet";
-			link.href = "/journal/people?p=" + (index+1);  // "/journal/" is included just in case the link is put in a weird place
-			if (occurrence.innerHTML.trim() != "") {
-				link.innerHTML = occurrence.innerHTML;
-				occurrence.innerHTML = "";
+			link.href = "/journal/people?p=" + index;  // "/journal/" is included just in case the link is put in a weird place
+			if (occurrence.textContent.trim() != "") {
+				link.textContent = occurrence.textContent;
+				occurrence.textContent = "";
 			} else if (occurrence.dataset.use) {
-				link.innerHTML = person[occurrence.dataset.use];
+				link.textContent = person[occurrence.dataset.use];
 			} else {
-				link.innerHTML = person.name;
+				link.textContent = person.name;
 			}
 			occurrence.appendChild(link);
 		});
