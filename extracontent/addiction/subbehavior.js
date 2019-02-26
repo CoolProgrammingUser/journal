e46a534ea11c506788d8072812ebef8ba16e0052
Robert <ambitiousbusinessowner@gmail.com>
2019-02-26 08:37:46 -0600

    tried fixing the addiction pop-up

--- extracontent/addiction/subbehavior.js
+++ extracontent/addiction/subbehavior.js
@@ -2,7 +2,7 @@ S.onLoad(function () {
 	let url = new URL(window.location.href);
 	console.log(url);
 	console.log(S.getEnd(url.href.split("/")));
-	if (S.getEnd(url.href.split("/")) != "index.html") {
+	if (S.getEnd(url.href.split("/")) != "index.html" && (S.getEnd(url.href.split("/")) != "") {
 		S.makeDialog("Are you sure that you want to read this? Once you read it, it can't be unread.",
 			"Yes",
 			["No", function () {
