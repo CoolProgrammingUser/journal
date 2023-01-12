c7f1a52d13716a77f58fb38f9714fcab0890c6ee
Robert <ambitiousbusinessowner@gmail.com>
2023-01-11 22:18:29 -0600

    made refinements & added more content

--- topics.js
+++ topics.js
@@ -1,3 +1,5 @@
 var topics = [{}];
 topics[1] = { title: "Determination", summary: "When I won't let anything prevent me from achieving my goal.", keywords: ["determined", "persistence", "persistent", "unstoppable", "focused"] };
 topics[2] = { title: "Recipes", summary: "How to make certain delectable foods.", keywords: ["food", "dessert", "delectable", "yummy", "ice cream", "oreo", "cake", "chocolate"] };
+topics[3] = { title: "Honesty", summary: "How I don't lie to anyone.", keywords: ["honest", "truth", "lie", "lying"] };
+topics[4] = { title: "Sneakiness", summary: "When I hide my master plan behind a convincing fascade of innocence.", keywords: ["sneaky", "mischevious", "evil", "plotting", "scheming", "guilty", "innocent", "orchestrating", "masterminding", "secret"] };
